76b359278fc5011c4ad01e0457466f23d6932e16
pedroM <pereleinteractive@gmail.com>
2022-02-22 12:31:45 +0100

    =incluimos encriptado y resto hasta borrar

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt= require('bcryptjs')
 
+const encriptar= async (password)=>{
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password,salt)
+  return hash
+}
 
 const mysql= require('mysql')
 const conn=mysql.createConnection({
@@ -16,28 +22,42 @@ router.get('/', (req, res, next)=> {
 router.get('/insertarUsuario', (req, res, next)=> { 
   res.render('insertarUsuario')
 });
-router.post('/insertarUsuario',(req,res,next)=>{
-  conn.query('insert into usuarios set? ',[req.body],(err,result)=>{
+router.post('/insertarUsuario',async(req,res,next)=>{
+  const user={
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  }
+  user.password= await encriptar(req.body.password)
+  await conn.query('insert into usuarios set? ',[user],(err,result)=>{
     res.redirect('/listarUsuario')
   })
 })
-router.get('/listarUsuario',(req,res,next)=>{
-  conn.query('select * from usuarios',(err,result)=>{
+router.get('/listarUsuario', async(req,res,next)=>{
+  await conn.query('select * from usuarios',(err,result)=>{
     res.render('listarUsuarios',{
       data:result
     })
   })
 })
-router.get('/editar/:id',(req,res,next)=>{
-  conn.query('select * from usuarios where id=?',[req.params.id],(err,result)=>{
-    
+router.get('/editarUsuario/:id', async (req,res,next)=>{
+  await conn.query('select * from usuarios where id=?',[req.params.id],(err,result)=>{
+    res.render('editarUsuario',{
+      data:result[0]
+    })
   })
 })
-
+router.post('/editarUsuario/:id', async (req,res,next)=>{
+  await conn.query('update usuarios set? where id=?',[req.body,req.params.id],(err,result)=>{
+    res.redirect('/listarUsuario')
+})
+})
 router.get('insertarCoche',(req,res,next)=>{
   res.render('insertarCoche')
 })
-router.get('/listarUsuarios',(req,res,next)=>{
-  
+router.get('/borrarUsuario/:id',async (req,res,next)=>{
+  await conn.query('delete from usuarios where id=?', [req.params.id],(err,result)=>{
+    res.redirect('/listarUsuarios')
+  })
 })
 module.exports = router;
