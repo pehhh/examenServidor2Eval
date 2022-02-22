01856b754bf3497046bbe21e8a60ec34eb4ca4e8
pedroM <pereleinteractive@gmail.com>
2022-02-22 13:14:16 +0100

    app operativa y con estilos

--- routes/index.js
+++ routes/index.js
@@ -10,15 +10,17 @@ const encriptar= async (password)=>{
 
 const mysql= require('mysql')
 const conn=mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'password',
-  database:'coches'
+  host:process.env.DES_HOST,
+  user:process.env.DES_USER,
+  password:process.env.DES_PASS,
+  database:process.env.DES_DB
 })
 /* GET home page. */
 router.get('/', (req, res, next)=> {
   res.render('index');
 });
+
+//usuarios
 router.get('/insertarUsuario', (req, res, next)=> { 
   res.render('insertarUsuario')
 });
@@ -57,7 +59,45 @@ router.get('insertarCoche',(req,res,next)=>{
 })
 router.get('/borrarUsuario/:id',async (req,res,next)=>{
   await conn.query('delete from usuarios where id=?', [req.params.id],(err,result)=>{
-    res.redirect('/listarUsuarios')
+    res.redirect('/listarUsuario')
   })
 })
+// coches
+router.get('/insertarCoche', (req, res, next)=> { 
+  res.render('insertarCoche')
+});
+router.post('/insertarCoche',async(req,res,next)=>{
+  console.log(req.body)
+  await conn.query('insert into coches set? ',[req.body],(err,result)=>{
+    res.redirect('/listarCoche')
+  })
+})
+router.get('/listarCoche', async(req,res,next)=>{
+  await conn.query('select * from coches',(err,result)=>{
+    res.render('listarCoche',{
+      data:result
+    })
+  })
+})
+router.get('/editarCoche/:id', async (req,res,next)=>{
+  await conn.query('select * from coches where id=?',[req.params.id],(err,result)=>{
+    res.render('editarCoche',{
+      data:result[0]
+    })
+  })
+})
+router.post('/editarCoche/:id', async (req,res,next)=>{
+  await conn.query('update coches set? where id=?',[req.body,req.params.id],(err,result)=>{
+    res.redirect('/listarCoche')
+})
+})
+router.get('insertarCoche',(req,res,next)=>{
+  res.render('insertarCoche')
+})
+router.get('/borrarCoche/:id',async (req,res,next)=>{
+  await conn.query('delete from coches where id=?', [req.params.id],(err,result)=>{
+    res.redirect('/listarCoche')
+  })
+})
+
 module.exports = router;
